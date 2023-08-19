96cb34d1b81484621d10c1744a5d442b42cb0880
asbernoma <naruto5235682@gmail.com>
2023-08-19 12:21:58 -0400

    finish unit testing

--- Unittesting/fetching.test.js
+++ Unittesting/fetching.test.js
@@ -1,17 +1,19 @@
 const axios = require('axios');
-const getUserProfile = require('./getUserProfile');
+const getUserProfile = require('./fetching');
 
-jest.mock(axios);
+jest.mock(axios)
 
 test('fetches user profile from the API', async () => {
   // Setup
-  const mockUser = { id: 1, name: 'John Doe' };
-  axios.get.mockResolvedValue({ data: mockUser });
+//   const mockUser = { id: 1, name: 'John Doe' };
+//   axios.get.mockResolvedValue({ data: mockUser });
 
-  // Exercise
-  const user = await getUserProfile(1);
-
+//   // Exercise
+//   const user = await getUserProfile(1);
+  const mockResponse = { data: { id: 1, name: 'John Doe', }, };
   // Verify
   expect(user).toEqual(mockUser);
   expect(axios.get).toHaveBeenCalledWith('https://api.example.com/users/1');
 });
+
+//I should check the fetching and how it works
